ece369639853a87143b1111e756b8bfc9558d8e7
w595624187 <w595624187@163.com>
2019-06-18 23:55:34 +0800

    第二次修改代码

--- src/testgit/git.java
+++ src/testgit/git.java
@@ -3,5 +3,8 @@ package testgit;
 public class git {
 	public static void main(String[]args) {
 		System.out.println("Hello Git !");
+		int a=1;
+		int b=2;
+		System.out.println("a+b="+(a+b));
 	}
 }
